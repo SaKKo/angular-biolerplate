6d056a8033fa1c189120fec62f46d1fe83d649f1
sakko <saklism@gmail.com>
2015-11-22 11:12:00 +0700

    added sidebar directive

--- src/application/application.ctrl.js
+++ src/application/application.ctrl.js
@@ -1,6 +1,7 @@
 angular.module('app')
 .controller('ApplicationCtrl', ['$scope', '$mdSidenav', function ($scope,$mdSidenav) {
   var self = this;
+  self.title = "Created by SaKKo"
   self.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   }
---
+++ src/sidebar/sidebar.ctrl.js
@@ -0,0 +1,8 @@
+angular.module('app').controller('SidebarCtrl', ['$scope', function($scope) {
+  
+}])
+.directive('mySidebar', function() {
+  return {
+    templateUrl: 'src/sidebar/sidebar.html'
+  };
+});
