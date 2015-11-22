2c316f00ece7bcdc822e2d681ee2cfab04ee1845
sakko <saklism@gmail.com>
2015-11-22 12:51:45 +0700

    added protractor screenshot

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,3 +1,5 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['specs/home-spec.js'],
@@ -5,5 +7,14 @@ exports.config = {
     browserName: 'firefox'
   }, {
     browserName: 'chrome'
-  }]
+  }],
+  onPrepare: function() {
+      jasmine.getEnv().addReporter(
+        new HtmlScreenshotReporter({
+          dest: 'protractor/tmp/screenshots',
+          filename: 'my-report.html',
+          captureOnlyFailedSpecs: true
+        })
+      );
+   }
 };
